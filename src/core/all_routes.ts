6f36b85eb3e0b48938fe8467c3b4bba60ccfc50a
Sebastian Mandal <sebastian.mandal@icloud.com>
2021-08-04 04:01:54 +0200

    GitHub and CompetentProgrammer redirect

--- src/core/all_routes.ts
+++ src/core/all_routes.ts
@@ -4,9 +4,16 @@ import indexPage from "../pages/index-page";
 // redirect routes
 import mandoRedirect from "../redirects/mando";
 import teboneRedirect from "../redirects/tebone";
+import CompRedirect from "../redirects/competent";
+import GitHubRedirect from "../redirects/github";
 
 export default {
+	//render
 	indexPage,
+
+	//redirect
 	mandoRedirect,
 	teboneRedirect,
+	CompRedirect,
+	GitHubRedirect,
 };
