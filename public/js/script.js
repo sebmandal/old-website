9243679e3c165a3f35910e839780e4474a24a049
Sebastian Mandal <sebastian.mandal@icloud.com>
2021-08-07 07:52:32 +0200

    why would I make people wait when they dont have to

--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-// credit: https://stackoverflow.com/questions/14434604/i-want-to-delay-a-link-for-a-period-of-500-with-javascript
-
-// delaying a redirect (for letting animations on mobile run before redirecting)
-function delay(URL) {
-	setTimeout(function () {
-		window.open(URL, "_blank"); // this is changed from the SO answer
-	}, 500);
-}
